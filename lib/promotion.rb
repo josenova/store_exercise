5a47ad52b76e9bf0566872ec44a4b21a4d88491f
Jose Nova <josenova@Joses-MacBook-Pro.local>
2017-05-01 20:36:39 +0200

    Comments

--- lib/promotion.rb
+++ lib/promotion.rb
@@ -11,20 +11,21 @@ class Promotion
 
   # This class contains the pricing rules (renamed promotions).
   # Using this structure we are able to create 2x1, 3x2, bulk discounts, & more using only this class.
+  # The `discount` and `affected` attributes are percentages represented as decimals.
 
   # For example:
 
   ## Promotion 2x1
-  # Item: VOUCHER
+  # Item: "VOUCHER"
   # Min Qty: 2
-  # Affected: 50% -> Half of the items (divide by 2, get integer)
-  # Discount: 100% -> Means the items affected are free
+  # Affected: 0.5 -> Half of the items (divide by 2, get integer)
+  # Discount: 1 -> Means the items affected are discounted by 100% (free)
 
 
   ## Promotion Bulk
-  # Item: TSHIRT
+  # Item: "TSHIRT"
   # Min Qty: 3
-  # Affected: 100% -> All of the tshirts get price reduction
-  # Discount: 5% -> The discount is for 1€ since their price is 20.00€
+  # Affected: 1 -> All of the tshirts get price reduction
+  # Discount: 0.05 -> The discount is for 1€ since their price is 20.00€
 
 end
